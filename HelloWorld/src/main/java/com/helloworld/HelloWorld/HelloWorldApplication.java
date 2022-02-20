a9afec177df0c1785830a773aa018fb125b5477b
igorgll <igorlimagn@gmail.com>
2022-02-20 01:27:06 -0300

    Atividade 1 e 2

--- HelloWorld/src/main/java/com/helloworld/HelloWorld/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/helloworld/HelloWorld/HelloWorldApplication.java
@@ -3,20 +3,22 @@ package com.helloworld.HelloWorld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@RestController 
+@RequestMapping
 public class HelloWorldApplication {
 	
 	@GetMapping ("/rota1")
 	public String ex1() {
-		return "Peristência e Orientação aos Detalhes";
+		return "<h1> Peristência e Orientação aos Detalhes </h1>";
 	}
 
 	@GetMapping ("/rota2")
 		public String ex2() {
-			return "Dominar MySQL básico e dominar SpringBoot básico"; 
+			return "<h1> Dominar MySQL básico e dominar SpringBoot básico </h1>"; 
 	} 
 		
 	public static void main(String[] args) {
